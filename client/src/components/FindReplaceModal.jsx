21b4920d82edbe2b350a9acaeae0e413a8ca71bc
Krutibash Mohapatra <krutibashmohapatra7@gmail.com>
2025-02-27 14:30:20 +0530

    demo commit

---
+++ client/src/components/FindReplaceModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FindReplaceModal = () => {
+  return (
+    <div>FindReplaceModal</div>
+  )
+}
+
+export default FindReplaceModal
---
+++ client/src/components/RowHeaders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RowHeaders = () => {
+  return (
+    <div>RowHeaders</div>
+  )
+}
+
+export default RowHeaders
